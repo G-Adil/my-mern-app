27ee614a76491c3c98686a9fc048e8ab77326e89
G-Adil <g.adil@users.noreply.github.com>
2018-11-27 22:03:22 +0100

    test Azure DevOps CI/CD

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -3,7 +3,7 @@ import React from 'react'
 const App = () =>  {
     return (
         <div>
-            Hi There! <a href="/test/route">Click and see</a>
+            Hi There! <a href="/test/route">Click and see the server</a>
         </div>
     );
 }
