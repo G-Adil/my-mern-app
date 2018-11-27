ae0f186c25c9937aa1907a141b28881ba10876eb
G-Adil <g.adil@users.noreply.github.com>
2018-11-27 12:03:05 +0100

    Hello React!

---
+++ client/build/precache-manifest.6deac4f0df5be444c60cf718864c0e8e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "34c2ca4b9a816bb1ae36",
+    "url": "/static/js/main.34c2ca4b.chunk.js"
+  },
+  {
+    "revision": "2dd891c45704ef41f3d5",
+    "url": "/static/js/1.2dd891c4.chunk.js"
+  },
+  {
+    "revision": "34c2ca4b9a816bb1ae36",
+    "url": "/static/css/main.b3d99ddf.chunk.css"
+  },
+  {
+    "revision": "f3ed09e29bb0ef3694bb2056ad9272f6",
+    "url": "/index.html"
+  }
+];
